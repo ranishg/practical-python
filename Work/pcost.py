c5bfc102fabd5b52aa2a385c97e2b006bc6dea29
Ranish George <ranish.george@ingka.ikea.com>
2024-04-20 19:55:40 +0200

    Add code to calculate total cost in pcost.py

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,12 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0
+with open("Work/Data/portfolio.csv", "r") as f:
+    headers = next(f).strip().split(",")
+    for line in f:
+        row = line.strip().split(",")
+        total_cost += int(row[1]) * float(row[2])
+
+    print("Total cost: ", total_cost)
