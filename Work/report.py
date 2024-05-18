f25cff727d1cdba0e10a4c563697ea934cd5f73c
Ranish George <ranish.george@ingka.ikea.com>
2024-05-18 22:59:51 +0200

    chore: Add make_report function to generate portfolio report

--- Work/report.py
+++ Work/report.py
@@ -32,6 +32,19 @@ def read_prices(filename):
     return prices
 
 
+def make_report(portfolio, prices):
+    report = []
+    for holding in portfolio:
+        name = holding["name"]
+        shares = holding["shares"]
+        price = prices[name]
+        change = price - holding["price"]
+        format_price = "$" + f"{price:.2f}"
+        report_item = (name, shares, format_price, change)
+        report.append(report_item)
+    return report
+
+
 def main(argv):
     if len(argv) == 3:
         portfolio_filename = argv[1]
@@ -55,6 +68,14 @@ def main(argv):
 
     gain_loss = current_value - total_cost
     print("Gain/Loss: ", gain_loss)
+
+    report = make_report(portfolio, prices)
+
+    headers = ("Name", "Shares", "Price", "Change")
+    print("%10s %10s %10s %10s" % headers)
+    print(("-" * 10 + " ") * len(headers))
+    for item in report:
+        print("%10s %10d %10s %10.2f" % item)
 
 
 if __name__ == "__main__":
